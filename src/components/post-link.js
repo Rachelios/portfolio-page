a7c4af6dd66c6e75f78f58c77ae3afbfb0974baa
Rachelios <lenguyenbichngoc95@gmail.com>
2021-06-26 17:14:33 +0800

    modify navigation

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -7,7 +7,7 @@ const PostLink = ({ post }) => (
       {!!post.frontmatter.thumbnail && (
         <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
       )}
-    </Link>
+    </a>
     <header>
       <h2 className="post-title">
         <Link to={post.frontmatter.path} className="post-link">
