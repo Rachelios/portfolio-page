6bfaebfd12a2e950bb6631a0d02dee174a94b288
Rachelios <lenguyenbichngoc95@gmail.com>
2021-06-26 16:27:07 +0800

    modify confige

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Rachel Portfolio`,
+        short_name: `Rachel`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
