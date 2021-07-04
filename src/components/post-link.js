d767de87d0cdd69d285d6328fd92cff55927a9d9
Rachelios <lenguyenbichngoc95@gmail.com>
2021-07-04 22:10:43 +0800

    📔 update

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -10,7 +10,7 @@ const PostLink = ({ post }) => (
     </a>
     <header>
       <h2 className="post-title">
-        <<Link to={post.frontmatter.path} className="post-link">
+        <Link to={post.frontmatter.path} className="post-link">
           {post.frontmatter.title}
           </Link>
       </h2>
