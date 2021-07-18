32f7c45f1973b9643fdce8ede020fd967b36a5ce
Rachelios <lenguyenbichngoc95@gmail.com>
2021-07-18 16:01:36 +0800

    📔 update

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -3,14 +3,14 @@ import { Link } from "gatsby"
 
 const PostLink = ({ post }) => (
   <article className="card ">
-    <a href= {`https://heyiamrachel.com/${post.frontmatter.path}`} >
+    <a href= {`https://heyiamrachel.com${post.frontmatter.path}`} >
       {!!post.frontmatter.thumbnail && (
         <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
       )}
     </a>
     <header>
       <h2 className="post-title">
-        <a href= {`https://heyiamrachel.com/${post.frontmatter.path}`} className="post-link">
+        <a href= {`https://heyiamrachel.com${post.frontmatter.path}`} className="post-link">
             {post.frontmatter.title}
            </a>
       </h2>
